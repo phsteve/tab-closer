90264f1d4d54bb127d175efc520a7de76c77943e
Steve Katz <katz.stephenj@gmail.com>
2021-03-18 20:08:49 -0400

    remove demo code, get tab counts

--- popup.js
+++ popup.js
@@ -1,20 +1,52 @@
-let changeColor = document.getElementById("changeColor");
+let tabInfo = document.getElementById("tabInfo");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-})
+let freqs = {}
 
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+// tab example
+//{"active":false,
+// "audible":false,
+// "autoDiscardable":true,
+// "discarded":false,
+// "favIconUrl":"https://developer.chrome.com/images/meta/favicon-32x32.png",
+// "groupId":-1,
+// "height":737,
+// "highlighted":false,
+// "id":1690,
+// "incognito":false,
+// "index":4,
+// "mutedInfo":{"muted":false},
+// "pinned":false,
+// "selected":false,
+// "status":"complete",
+// "title":"chrome.tabs - Chrome Developers",
+// "url":"https://developer.chrome.com/docs/extensions/reference/tabs/#method-query",
+// "width":714,
+// "windowId":602},
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
+chrome.tabs.query({}, (tabs) => {
+  tabs.forEach(tab => {
+    try {
+      let url = new URL(tab.url)
+      console.log(`got ${url.hostname}`)
+      if (freqs.hasOwnProperty(url.hostname)) {
+        freqs[url.hostname]++
+      } else {
+        freqs[url.hostname] = 1
+        console.log(JSON.stringify(freq))
+      }
+    } catch(err) {
+      console.log(`skipping invalid url: ${tab.url}`)
+      return
+    }
 
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
   })
-}
+  sortedTabs = []
+  for (let hostname in freqs) {
+    sortedTabs.push([hostname, freqs[hostname]])
+  };
+  sortedTabs.sort((a, b) => {
+    return b[1] - a[1]
+  });
+  console.log(JSON.stringify(sortedTabs))
+  tabInfo.innerHTML = JSON.stringify(sortedTabs)
+});
