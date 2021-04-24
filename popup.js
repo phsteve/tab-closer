b4414ec3dd04874d6048b011b98249ac9a0674f7
Steve Katz <katz.stephenj@gmail.com>
2021-04-24 17:10:22 -0400

    make it look better

--- popup.js
+++ popup.js
@@ -4,39 +4,31 @@ chrome.tabs.query({}, (tabs) => {
   for (const tab of tabs) {
     try {
       const tabUrl = tab.url ? new URL(tab.url) : { hostname: "" }
-      curr = counts.get(tabUrl.hostname)
+      let curr = counts.get(tabUrl.hostname)
       if (curr) {
         counts.set(tabUrl.hostname, curr + 1)
       } else {
         counts.set(tabUrl.hostname, 1)
       }
-
     } catch(err) {
       console.log(`err: ${err}`)
       console.log(`skipping invalid url from tab ${tab.id}: -${tab.url}-`)
     }
   }
-  // convert counts to array so we can sort it by hostname count
-  sortedTabs = []
-  console.log('counts:')
-  console.log(JSON.stringify(counts))
-  for (const [hostname, count] of counts) {
-    sortedTabs.push([hostname, count])
-  };
-  sortedTabs.sort((a, b) => {
+  sortedTabs = Array.from(counts).sort((a, b) => {
     // sort descending
     return b[1] - a[1]
-  });
-  console.log('sorted tabs: ' + sortedTabs)
+  })
   for (const tab of sortedTabs) {
     li = document.createElement("li")
     li.setAttribute("id", tab[0])
-    button = document.createElement("button")
+    const button = document.createElement("button")
     button.innerHTML = "Close"
     button.setAttribute("hostname", tab[0])
+    button.setAttribute("style", "background-color:f14e4e")
     button.addEventListener('click', closeTabsByHostname)
     const displayName = tab[0] ? tab[0] : "(empty URL)"
-    text = document.createTextNode(`${tab[1]} from ${displayName}`)
+    const text = document.createTextNode(`${tab[1]} from ${displayName}`)
     li.appendChild(button)
     li.appendChild(text)
     tabList.appendChild(li)
